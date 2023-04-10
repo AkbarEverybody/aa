fa20a92d5904a2c976881491bd7b0def36fe0ae4
AkbarEverybody <akbarpribadi549@gmail.com>
2023-04-10 16:44:46 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function myFunction(type, text, color) {
+  const el = document.createElement(type);
+  el.innerText = text;
+  el.style.color = color;
+  document.body.append(el);
+  return {
+    el,
+    setText(text) {
+      el.innerText = text;
+    },
+    setColor(color) {
+      el.style.color = color;
+    },
+  };
+}
+myFunction("h1", "hehhe", "red");
